6a73ef4c74daee626618f79251200ebefea5b566
Gagan Solur Venkatesh <gaganyv8@gmail.com>
2025-11-05 23:55:17 -0600

    first commit

---
+++ fraud-detection-system/src/main/java/com/gsphere/fraud_detection_system/FraudDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gsphere.fraud_detection_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionSystemApplication.class, args);
+	}
+
+}
